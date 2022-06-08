b006aed4e2e2b5bf0ae26e885b448aa6d7ba5e23
Daniel Gorbatov <dg20@uw.edu>
2022-06-08 12:59:02 -0700

    Add conversion and motor helper class's

---
+++ Conversion.java
@@ -0,0 +1,17 @@
+package frc.robot.util;
+
+public class Conversion {
+    public static double ticksToMeters(double ticks, double wheelDiamater) {
+        return (ticks / 2048.0) * ((wheelDiamater * Math.PI) / 39.37);
+    }
+
+    public static double normalizeGyro(double currAngle) {
+        if (currAngle < -180) {
+            return 180 + (currAngle + 180);
+        } else if (currAngle > 180) {
+            return -180 + (currAngle - 180);
+        } else {
+            return currAngle;
+        }
+    }
+}
---
+++ MotorHelper.java
@@ -0,0 +1,46 @@
+package frc.robot.util;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public class MotorHelper {
+    public static WPI_TalonFX createFalconMotor(int canID, int currentLimit, NeutralMode neutralMode, TalonFXInvertType inversion) {
+        WPI_TalonFX motor = new WPI_TalonFX(canID);
+        motor.configFactoryDefault();
+        motor.setSelectedSensorPosition(0);
+        motor.setInverted(inversion);
+        motor.configSupplyCurrentLimit(createCurrentLimt(currentLimit));
+        motor.setNeutralMode(neutralMode);
+        return motor;
+    }
+
+    public static WPI_TalonFX createFalconMotor(int canID, int currentLimit, TalonFXInvertType inversion) {
+        WPI_TalonFX motor = new WPI_TalonFX(canID);
+        motor.configFactoryDefault();
+        motor.setSelectedSensorPosition(0);
+        motor.setInverted(inversion);
+        motor.configSupplyCurrentLimit(createCurrentLimt(currentLimit));
+        return motor;
+    }
+
+    public static WPI_TalonFX createFalconMotor(int canID) {
+        WPI_TalonFX motor = new WPI_TalonFX(canID);
+        motor.configFactoryDefault();
+        motor.setSelectedSensorPosition(0);
+        return motor;
+    }
+
+    public static WPI_TalonFX createFalconMotor(int canID, TalonFXInvertType inversion) {
+        WPI_TalonFX motor = new WPI_TalonFX(canID);
+        motor.configFactoryDefault();
+        motor.setSelectedSensorPosition(0);
+        motor.setInverted(inversion);
+        return motor;
+    }
+
+    public static SupplyCurrentLimitConfiguration createCurrentLimt(int currentLimit) {
+        return new SupplyCurrentLimitConfiguration(true, currentLimit-5, currentLimit, 0.75);
+    }
+}
