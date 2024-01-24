c7da309451130fa0a179ea1ea350bcd29b25401a
Daniel Gorbatov <dzgorbatov@gmail.com>
2024-01-23 17:25:25 -0800

    goofy

--- Swerve/SwerveTemplates/VisionBaseSwerve.java
+++ Swerve/SwerveTemplates/VisionBaseSwerve.java
@@ -23,7 +23,7 @@ public abstract class VisionBaseSwerve extends BaseSwerve {
                 getPose(),
                 new Pose2d(0, 0, getGyro().getAngle()));
 
-        setOldPose(this.odom.getEstimatedPosition());
+        setPrevPose(this.odom.getEstimatedPosition());
     }
 
     @Override
