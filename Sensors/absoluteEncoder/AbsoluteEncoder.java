00be929358883fd7bb88a160aa095beddaa50505
Daniel Gorbatov <dzgorbatov@gmail.com>
2024-02-21 18:27:16 -0800

    absolute encoder

--- Sensors/absoluteEncoder/AbsoluteEncoder.java
+++ Sensors/absoluteEncoder/AbsoluteEncoder.java
@@ -6,14 +6,21 @@ import edu.wpi.first.wpilibj.DutyCycleEncoder;
 public class AbsoluteEncoder {
     private DutyCycleEncoder encoder;
     private double offset;
+    private int inversion;
 
-    public AbsoluteEncoder(int id, double offset) {
+    public AbsoluteEncoder(int id, double offset, boolean inversion) {
         encoder = new DutyCycleEncoder(id);
         this.offset = offset;
+
+        if (inversion) {
+            this.inversion = -1;
+        } else {
+            this.inversion = 1;
+        }
     }
 
     public double getPosition() {
-        return encoder.get() * 360 - offset;
+        return encoder.get() * 360 * inversion - offset;
     }
 
     public double getNormalizedPosition() {
